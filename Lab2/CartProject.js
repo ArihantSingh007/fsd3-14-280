1c62546ee2b146bc331ba0a7d10543f4f0951e81
Dhaneshwar Kumar <dhannesh@gmail.com>
2026-08-18 10:11:15 +0530

    add app.js v1

--- Lab2/CartProject.js
+++ Lab2/CartProject.js
@@ -1,6 +1,7 @@
 import readline from "readline/promises";
 import { stdin, stdout } from "process";
 import { readFile, writeFile } from "fs/promises";
+import { log } from "console";
 
 // Database using file starts
 const FILE = "product.json";
@@ -35,6 +36,35 @@ const displayCart = async () => {
   console.log(`Total payble amount Rs. ${total}`);
 };
 
+const removeProduct = async (pid) => {
+  const cart = await getCart();
+  let x = cart.length;
+  //  const isFoundInCart = cart.find((item) => item.id === pid);
+  const newProducts = cart.filter((item) => item.id !== pid);
+  let y = newProducts.length;
+  if (x > y) {
+    console.log(`Product with id ${pid} is removed from cart`);
+    await saveCart(newProducts);
+  } else {
+    console.log(`Product with id ${pid} not found`);
+  }
+};
+const updateQuantity = async (pid, option = "-") => {
+  const cart = await getCart();
+  const isFoundInCart = cart.find((item) => item.id === pid);
+  if (isFoundInCart) {
+    if (isFoundInCart.qty == 1) {
+      await removeProduct(pid);
+    } else {
+      isFoundInCart.qty -= 1;
+      await saveCart(cart);
+    }
+    console.log(`${isFoundInCart.name} quantity updated from 🛒`);
+  } else {
+    console.log(`Product with id ${pid} not found`);
+  }
+};
+
 const main = async () => {
   let choice;
   const cin = readline.createInterface({ input: stdin, output: stdout });
@@ -63,12 +93,13 @@ const main = async () => {
           qty: Number(qty),
         });
         break;
-
       case 3:
-        console.log("remove product");
+        let pid = await cin.question("Enter product id:");
+        await removeProduct(Number(pid));
         break;
       case 4:
-        console.log("update quantity");
+        let pid2 = await cin.question("Enter product id and option(+/-):");
+        await updateQuantity(Number(pid2));
         break;
       case 5:
         console.log("checkout");
